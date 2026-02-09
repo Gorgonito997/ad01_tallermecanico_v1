0ce038a261762f670da981b3bdc37075c23ca2ea
diegocm <diegocm505@gmail.com>
2026-02-09 12:44:16 +0100

    Creada FuenteDatosMongoDb devolviendo null porque aun no están implementadas las clases

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/mongodb/FuenteDatosMongoDb.java
@@ -0,0 +1,25 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio.mongodb;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IClientes;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IFuenteDatos;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ITrabajos;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.IVehiculos;
+
+public class FuenteDatosMongoDb implements IFuenteDatos {
+
+    @Override
+    public IClientes crearClientes() {
+        return null;
+    }
+
+    @Override
+    public IVehiculos crearVehiculos() {
+        return null;
+    }
+
+    @Override
+    public ITrabajos crearTrabajos() {
+        return null;
+    }
+}
+
